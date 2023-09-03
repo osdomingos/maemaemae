1775033fdecb9eba57b5ee0745a8b79388388f25
osdomingos <osdomingos@gmail.com>
2023-09-03 02:26:17 -0300

    JS: Declaradas variáveis globais que apontam para elementos do documento HTML; CSS: Teste de cursor de espera sobre todo o corpo da página.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Variáveis globais
+const falasDasMaes = []
+const titulo = document.getElementById('titulo')
+const mensagens = document.getElementById('mensagens')
+const sobre = document.getElementById('sobre')
+
